7f62f4adcae899b913ac7c1a3168043c8071758b
LStandman <65296484+LStandman@users.noreply.github.com>
2023-11-27 19:20:05 +0200

    BNF :: Simplify the description of relations between parsers.
    Signed-off-by: LStandman <65296484+LStandman@users.noreply.github.com>

--- gribbler/yaml/BNF.hs
+++ gribbler/yaml/BNF.hs
@@ -22,9 +22,9 @@ infixl 1 `conv`
 infixl 1 `err`
 infixl 1 `et`
 infixl 1 `look_ahead`
-infixl 1 `on_success`
+infixl 1 `on_hit`
+infixl 1 `on_miss`
 infixl 1 `ou`
-infixl 1 `ou'`
 infixl 1 `except`
 
 data Result a b =
@@ -35,17 +35,17 @@ data Result a b =
 
 type Parser a b = a -> Result a b
 
-conv        :: Parser a b -> (b -> c) -> Parser a c
-err         :: Parser a b -> String -> Parser a b
-et          :: Semigroup b => Parser a b -> Parser a b -> Parser a b
-except      :: Parser a b -> Parser a b -> Parser a b
-look_ahead  :: Parser a b -> Parser a b -> Parser a b
-nul         :: Monoid b => Parser a b
-oom         :: Semigroup b => Parser a b -> Parser a b
-ou          :: Parser a b -> Parser a b -> Parser a b
-rep         :: Semigroup b => Int -> Parser a b -> Parser a b
-zom         :: Monoid b => Parser a b -> Parser a b
-zoo         :: Monoid b => Parser a b -> Parser a b
+conv       :: Parser a b -> (b -> c) -> Parser a c
+err        :: Parser a b -> String -> Parser a b
+et         :: Semigroup b => Parser a b -> Parser a b -> Parser a b
+except     :: Parser a b -> Parser a b -> Parser a b
+look_ahead :: Parser a b -> Parser a b -> Parser a b
+nul        :: Monoid b => Parser a b
+oom        :: Semigroup b => Parser a b -> Parser a b
+ou         :: Parser a b -> Parser a b -> Parser a b
+rep        :: Semigroup b => Int -> Parser a b -> Parser a b
+zom        :: Monoid b => Parser a b -> Parser a b
+zoo        :: Monoid b => Parser a b -> Parser a b
 
 instance Functor (Result a)
   where
@@ -53,33 +53,30 @@ instance Functor (Result a)
     fmap _ Miss      = Miss
     fmap _ (Error e) = Error e
 
-ou' :: Result a b -> Result a b -> Result a b
-ou' (Hit ctx) _ = Hit ctx
-ou' Miss      r = r
-ou' (Error e) _ = Error e
+on_miss :: Result a b -> Result a b -> Result a b
+on_miss Miss r2 = r2
+on_miss r1   _  = r1
 
-ou f g = \ x -> f x `ou'` g x
+ou f g = \ x -> f x `on_miss` g x
 
-on_success :: Result a b -> ((a, b) -> Result a b) -> Result a b
-on_success (Hit ctx) f = f ctx
-on_success Miss      _ = Miss
-on_success (Error e) _ = Error e
+on_hit :: Result a b -> ((a, b) -> Result a b) -> Result a b
+on_hit (Hit ctx) f = f ctx
+on_hit r         _ = r
 
 cat :: Semigroup b => Parser a b -> (a, b) -> Result a b
 cat f (i, o) = fmap (o <>) $ f i
 
-et f g = \ x -> f x `on_success` cat g
+et f g = \ x -> f x `on_hit` cat g
 
 rep 1 f = f
 rep n f = f `et` rep (n - 1) f
 
-except' :: Result a b -> Result a b -> Result a b
-except' (Hit ctx)  Miss       = Hit ctx
-except' (Hit _)    (Error e2) = Error e2
-except' (Error e1) _          = Error e1
-except' _          _          = Miss
+invert :: Result a b -> (a, b) -> Result a b
+invert Miss      ctx  = Hit ctx
+invert (Hit _)   _    = Miss
+invert (Error e) _    = Error e
 
-except f g = \ x -> f x `except'` g x
+except f g = \ x -> f x `on_hit` invert (g x)
 
 nul i = Hit (i, mempty)
 
@@ -91,18 +88,17 @@ zom f = oom f `ou` nul
 
 more :: Semigroup b => Parser a b -> (a, b) -> Result a b
 more f (i1, o1)  = case f i1 of
-  Hit ctx2  -> Hit $ fmap (o1 <>) ctx2
-  Miss      -> Hit (i1, o1)
-  Error e   -> Error e
-
-oom f = \ x -> f x `on_success` more (oom f)
-
-err f e = \ x -> f x `on_success` return (Error e)
-
-peek :: Parser a b -> (a, b) -> Result a b
-peek f (i, o) = case f i of
-  Hit _    -> Hit (i, o)
-  Miss     -> Miss
+  Hit ctx2 -> Hit $ fmap (o1 <>) ctx2
+  Miss     -> Hit (i1, o1)
   Error e2 -> Error e2
 
-look_ahead f g = \ x -> f x `on_success` peek g
+oom f = \ x -> f x `on_hit` more (oom f)
+
+err f e = \ x -> f x `on_hit` return (Error e)
+
+peek :: Parser a b -> (a, b) -> Result a b
+peek f (i1, o1) = case f i1 of
+  Hit _ -> Hit (i1, o1)
+  r2    -> r2
+
+look_ahead f g = \ x -> f x `on_hit` peek g
