53f2df95054bd3e565655b0311ec0f7be7b2b61d
LStandman <65296484+LStandman@users.noreply.github.com>
2024-03-16 23:34:06 +0200

    SHA2 :: Try to speed up bufferring by doing _unsafe_ array opertaions.
    Signed-off-by: LStandman <65296484+LStandman@users.noreply.github.com>

--- gribbler/Crypt/SHA2.hs
+++ gribbler/Crypt/SHA2.hs
@@ -13,10 +13,13 @@ module Crypt.SHA2(
     sha256sum1)
   where
 
+import Control.Monad
+import Data.Array.IO
 import Data.Array.Unboxed
 import Data.Bits
 import Data.List
 import Data.Word
+import GHC.IO
 
 type Hash = UArray Int Word32
 
@@ -72,18 +75,17 @@ sha256round v x = w
     u  = ixmap bounds_hash (\ i -> (i - 1) `mod` size_hash) v
     w  = u//[(0, t1 + t2), (4, d + t1)]
 
-sha256sched' :: UArray Int Word32 -> Int -> UArray Int Word32
-sha256sched' v i = v//[
-    ( j,
-      (lil_sigma1 $ deref (j - 2))  + deref (j - 7) +
-      (lil_sigma0 $ deref (j - 15)) + deref j)
-    | j <- [i, i + 1]]
-  where
-    deref k = v!(k `mod` size_block)
+sha256sched' :: IOUArray Int Word32 -> Int -> IO ()
+sha256sched' v i = 
+  readArray v (i - 2) >>= \ a ->
+    readArray v (i - 7) >>= \ b ->
+      readArray v (i - 15) >>= \ c ->
+        readArray v (i - 16) >>= \ d ->
+          writeArray v i (lil_sigma1 a  + b + lil_sigma0 c + d)
 
-sha256sched :: UArray Int Word32 -> UArray Int Word32
+sha256sched :: IOUArray Int Word32 -> IO ()
 sha256sched v =
-  foldl' (sha256sched') v [0, 2..14]
+  mapM_ (sha256sched' v) [16..63]
 
 sha256block :: Hash -> [Word32] -> Hash
 sha256block h v =
@@ -100,7 +102,7 @@ from_list m  = foldl' (f) 0 m1 : from_list m2
 
 int_sha256once h m = h'
   where
-    k        = [
+    k  = [
       0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
       0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
       0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
@@ -117,9 +119,9 @@ int_sha256once h m = h'
       0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
       0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
       0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2]
-    w        = concatMap (elems) $ take 4 $ iterate (sha256sched) $
-               listArray (0, size_block - 1) $ from_list m
-    h'       = sha256block h $! zipWith (+) k w
+    w  = unsafePerformIO $
+         newListArray (0, 63) (from_list m) >>= \ v -> sha256sched v >> getElems v
+    h' = sha256block h $! zipWith (+) k w
 
 sha256sum' :: Hash -> [Word8] -> Int -> Hash
 sha256sum' h m l =
